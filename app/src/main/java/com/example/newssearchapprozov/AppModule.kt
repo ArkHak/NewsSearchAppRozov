c56e7d63c0dcfc8c5d0004819d3398891fb6529d
Oleg Mysin <7nacu7@gmail.com>
2024-05-19 16:58:14 +0300

    part 2
    - adding dagger + hilt;
    
    part 2 closed;

---
+++ app/src/main/java/com/example/newssearchapprozov/AppModule.kt
@@ -0,0 +1,33 @@
+package com.example.newssearchapprozov
+
+import android.content.Context
+import com.example.news.database.NewsDatabase
+import com.example.newsapi.NewsApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNewsApi(): NewsApi {
+        return NewsApi(
+            baseUrl = BuildConfig.NEWS_API_BASE_URL,
+            apiKey = BuildConfig.NEWS_API_KEY,
+        )
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideNewsDatabase(@ApplicationContext context: Context): NewsDatabase {
+        return NewsDatabase(context)
+    }
+
+}
