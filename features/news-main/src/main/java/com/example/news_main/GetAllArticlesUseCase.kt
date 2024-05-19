1e2dd8d52133f0896df88813acd58b4ae32c452c
Oleg Mysin <7nacu7@gmail.com>
2024-05-19 13:50:44 +0300

    part 2
    - adding all articles loading;

--- features/news-main/src/main/java/com/example/news_main/GetAllArticlesUseCase.kt
+++ features/news-main/src/main/java/com/example/news_main/GetAllArticlesUseCase.kt
@@ -1,14 +1,26 @@
 package com.example.news_main
 
 import com.example.news_data.ArticlesRepository
-import com.example.news_data.models.Article
+import com.example.news_data.RequestResult
+import com.example.news_data.map
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import com.example.news_data.models.Article as DataArticle
 
 class GetAllArticlesUseCase(
     private val repository: ArticlesRepository
 ) {
 
-    operator fun invoke(): Flow<Article> {
+    operator fun invoke(): Flow<RequestResult<List<Article>>> {
         return repository.getAll()
+            .map { requestResult ->
+                requestResult.map { articles ->
+                    articles.map { it.toUiArticle() }
+                }
+            }
     }
+}
+
+private fun DataArticle.toUiArticle(): Article {
+    TODO("Not implemented")
 }
